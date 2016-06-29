c66296a9d7d40d7b540d9983442c1ee4c0d87893
jungleli <jungle216@126.com>
2016-06-29 22:56:44 +0800

    Init

---
+++ scripts/main.js
@@ -0,0 +1,70 @@
+/**
+ * Created by T440 on 2016/6/29.
+ */
+var CommentBox = React.createClass({
+  render: function() {
+    return (
+      <div className="commentBox">
+      	<h1>Comments</h1>
+        <CommentList data={this.props.data}/>
+        <CommentForm />
+    </div>
+    );
+  }
+});
+
+var CommentForm = React.createClass({
+  render: function() {
+    return (
+      <div className="commentForm">
+        Hello, world! I am a CommentForm.
+      </div>
+    );
+  }
+});
+
+var Comment = React.createClass({
+	rawMarkup: function() {
+   	 var md = new Remarkable();
+   	 var rawMarkup = md.render(this.props.children.toString());
+    	return { __html: rawMarkup };
+  	},
+  render: function() {
+    return (
+      <div className="comment">
+        <h2 className="commentAuthor">
+          {this.props.author}
+        </h2>
+        <span dangerouslySetInnerHTML={this.rawMarkup()} />
+      </div>
+    );
+  }
+});
+
+var CommentList = React.createClass({
+	render: function() {
+    var commentNodes = this.props.data.map(function(comment) {
+      return (
+        <Comment author={comment.author}>
+          {comment.text}
+        </Comment>
+      );
+    });
+    return (
+      <div className="commentList">
+        {commentNodes}
+      </div>
+    );
+  }
+});
+
+var data = [
+  {author: "11Pete Hunt", text: "This is one comment"},
+  {author: "Jordan Walke", text: "This is *another* comment"}
+];
+
+
+ReactDOM.render(
+<CommentBox data={data} />,
+  document.getElementById('content')
+);
